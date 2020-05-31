b76961af963875b0049f181769a5e3ebd5972273
Gaurav Gulati <ggulati3@gmail.com>
2020-05-30 18:44:45 -0700

    Add POST Route for Singup  w/ Authentication

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,23 +1,23 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const serviceAccount = require('/Users/gauravgulati/Desktop/socialape-3be06-firebase-adminsdk-k4y2y-dcd2544adb.json')
+const config = require('./config')
+const app = require('express')();
+const firebase = require('firebase')
+firebase.initializeApp(config)
 
 // admin.initializeApp();
-
-// Comment Line 5 and Uncomment below to test endpoints locally before deploying -- run 'firebase serve'
-const serviceAccount = require('/Users/gauravgulati/Desktop/socialape-3be06-firebase-adminsdk-k4y2y-dcd2544adb.json')
-
+// admin w/ credentials for testing locally -- take out credentials to deploy
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://socialape-3be06.firebaseio.com"
 });
 
-const express = require('express');
-const app = express();
+const db = admin.firestore();
 
 // get scream documents 
 app.get('/screams', (req,res) => {
-    admin
-        .firestore()
+    db
         .collection('screams')
         .orderBy('createdAt', 'desc')
         .get()
@@ -45,8 +45,7 @@ app.post('/scream', (req, res) => {
         createdAt : new Date().toISOString()
     }
 
-    admin
-        .firestore()
+    db
         .collection('screams')
         .add(newScream)
         .then((doc) => {
@@ -58,7 +57,50 @@ app.post('/scream', (req, res) => {
         })
 })
 
-// https://baseurl.com/screams --> bad 
-// https://baseurl.com/api/screams --> good
+// Signup routes
+app.post('/signup', (req,res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    }
+
+    //validate data
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`).get()
+        .then((doc) => {
+            if(doc.exists) {
+                return res.status(400).json({ hanlde : 'this handle is alraeady taken'})
+            } else {
+                return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+            }
+        })
+        .then((data) => {
+            userId = data.user.uid;
+            return data.user.getIdToken()
+        })
+        .then((idToken) => {
+            token = idToken;
+            const userCredenitals = {
+                handle : newUser.handle,
+                email : newUser.email,
+                createdAt : new Date().toISOString(),
+                userId : userId
+            };
+            return db.doc(`/users/${newUser.handle}`).set(userCredenitals) // create users document in collection of firebase db
+        })
+        .then(() => {
+            return res.status(201).json({ token: token});
+        })
+        .catch((err) => {
+            console.error(err);
+            if(err.code === "auth/email-already-in-use") {
+                return res.status(400).json({email : "Email already in use."})
+            } else {
+                return res.status(500).json({error : err.code})
+            }
+        })
+})
 
 exports.api = functions.https.onRequest(app);
