07d8fef9756df7bf1ff37eb0376b25fc97b4db3c
Gaurav Gulati <ggulati3@gmail.com>
2020-05-31 15:00:52 -0700

    Added Firebase Authentication Middleware to Screams POST Request

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -37,12 +37,42 @@ app.get('/screams', (req,res) => {
         .catch(err => console.error(err))
 })
 
+// Firebase Authentication Middleware to Check User is logged in and has appropriate token
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found.')
+        return res.status(403).json({ error : 'Unauthorized'})
+    }
 
-app.post('/scream', (req, res) => {
+    admin.auth().verifyIdToken(idToken)
+        .then((decodedToken) => {
+            req.user = decodedToken;
+            console.log('this is the Decoded Token: ', decodedToken);
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then((data)=> {
+            req.user.handle = data.docs[0].data().handle;
+            return next(); // let code move on to (req, res function on post/get requests)
+        })
+        .catch((err) => {
+            console.error('Error while verifying token: ', err)
+            return res.status(403).json(err);
+        })
+}
 
+app.post('/scream', FBAuth, (req, res) => {
+    if(req.body.body.trim() === '') {
+        return res.status(400).json({ body: 'Body must not be empty. '});
+    }
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt : new Date().toISOString()
     }
 
